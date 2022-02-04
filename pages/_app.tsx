3fe1a4227e3c9f79aca044cf741508c1161d0d82
Ahmed Damasy <ahmeddamasy93@gmail.com>
2022-02-04 22:13:01 +0200

    Remove the server side injected css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,13 @@ import { ThemeProvider } from '@mui/material';
 import { themeDark, themeLight } from 'lib/theme';
 
 export default function MyApp ({Component, pageProps}) {
+  useEffect(() => {
+    // Remove the server side injected css
+    const jssStyles = document.querySelector('#jss-server-side');
+    if(jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
   
   return (
     <ThemeProvider theme={false ? themeDark : themeLight}>
