9c226ceb8d0273357881d7a2086c92f90d8b77a1
Ahmed Damasy <ahmeddamasy93@gmail.com>
2022-02-04 22:12:49 +0200

    provide a theme for the core app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import App from 'next/app';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 
-export default ({Component, pageProps}) => (
-  <Component {...pageProps} />
-)
+import { themeDark, themeLight } from 'lib/theme';
+
+export default function MyApp ({Component, pageProps}) {
+  
+  return (
+    <ThemeProvider theme={false ? themeDark : themeLight}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
