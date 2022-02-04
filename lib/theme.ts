9d864d6fb795c9699a711b5c9e75465b7fbdb9cf
Ahmed Damasy <ahmeddamasy93@gmail.com>
2022-02-04 22:03:42 +0200

    build app themes (light - dark)

---
+++ lib/theme.ts
@@ -0,0 +1,31 @@
+import { createTheme } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
+
+const themeDark = createTheme({
+  palette: {
+    primary: {
+      main: grey[200]
+    },
+    secondary: {
+      main: grey[400]
+    },
+    mode: 'dark'
+  }
+});
+
+const themeLight = createTheme({
+  palette: {
+    primary: {
+      main: grey[800]
+    },
+    secondary: {
+      main: grey[900]
+    },
+    mode: 'light'
+  }
+});
+
+export {
+  themeDark,
+  themeLight
+}
