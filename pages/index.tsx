c275120c480763942df24aaad4c98bcffd5aad08
Ahmed Damasy <ahmeddamasy93@gmail.com>
2022-02-05 00:25:15 +0200

    new jsx for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
-export default () => (
-  <div><p>Hello World</p></div>
-)
+import { Container, Button, Box, Typography } from "@mui/material";
+import Link from "next/link";
+
+export default function Index () {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js Example
+        </Typography>
+        <Link href="/about">
+          <Button variant="contained" color="primary">
+            Go to about page
+          </Button>
+        </Link>
+      </Box>
+    </Container>
+  )
+}
